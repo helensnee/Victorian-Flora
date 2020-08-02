ba9b5192209050a2427a35885d7797132b4848eb
helensnee <helensnee@lifesites.com.au>
2020-08-02 23:42:50 +1000

    Part 7 - Basic Backend

---
+++ admin/views/victorianfloras/tmpl/default.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_victorianflora
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted Access');
+?>
+<form action="index.php?option=com_victorianflora&view=victorianfloras" method="post" id="adminForm" name="adminForm">
+	<table class="table table-striped table-hover">
+		<thead>
+		<tr>
+			<th width="1%"><?php echo JText::_('COM_VICTORIANFLORA_NUM'); ?></th>
+			<th width="2%">
+				<?php echo JHtml::_('grid.checkall'); ?>
+			</th>
+			<th width="90%">
+				<?php echo JText::_('COM_VICTORIANFLORA_VICTORIANFLORAS_NAME') ;?>
+			</th>
+			<th width="5%">
+				<?php echo JText::_('COM_VICTORIANFLORA_PUBLISHED'); ?>
+			</th>
+			<th width="2%">
+				<?php echo JText::_('COM_VICTORIANFLORA_ID'); ?>
+			</th>
+		</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<td colspan="5">
+					<?php echo $this->pagination->getListFooter(); ?>
+				</td>
+			</tr>
+		</tfoot>
+		<tbody>
+			<?php if (!empty($this->items)) : ?>
+				<?php foreach ($this->items as $i => $row) : ?>
+
+					<tr>
+						<td>
+							<?php echo $this->pagination->getRowOffset($i); ?>
+						</td>
+						<td>
+							<?php echo JHtml::_('grid.id', $i, $row->id); ?>
+						</td>
+						<td>
+							<?php echo $row->BotanicalName; ?>
+						</td>
+						<td align="center">
+							<?php echo JHtml::_('jgrid.published', $row->published, $i, 'victorianfloras.', true, 'cb'); ?>
+						</td>
+						<td align="center">
+							<?php echo $row->id; ?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+			<?php endif; ?>
+		</tbody>
+	</table>
+</form>
